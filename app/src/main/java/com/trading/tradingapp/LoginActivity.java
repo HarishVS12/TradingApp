6985b0273e7f2cbac847639975d31a113965bf3c
harish <harishv783@gmail.com>
2020-01-12 17:13:39 +0530

    Modified LoginActivtiy.

---
+++ app/src/main/java/com/trading/tradingapp/LoginActivity.java
@@ -0,0 +1,103 @@
+package com.trading.tradingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.trading.tradingapp.Retrofit.CustomersAPI;
+import com.trading.tradingapp.Retrofit.LoginJSON;
+
+public class LoginActivity extends AppCompatActivity {
+
+    //Declarations
+    private EditText user,password;
+    private Button signIn;
+    private Boolean errorBool;
+    private CustomersAPI customersAPI;
+    Retrofit retrofit;
+    private String baseUrl , message;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Initialisations
+        user = findViewById(R.id.user);
+        password = findViewById(R.id.password);
+        signIn = findViewById(R.id.signIn);
+        baseUrl = "https://tradingapprestapi.000webhostapp.com/";
+
+        //Retrofit Initialisation
+        retrofit = new Retrofit.Builder()
+                        .baseUrl(baseUrl)
+                        .addConverterFactory(GsonConverterFactory.create())
+                        .build();
+        customersAPI = retrofit.create(CustomersAPI.class);
+
+        //SignIn Button OnClick
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final String username = user.getText().toString();
+                final String pass = password.getText().toString();
+                Login(username , pass);
+            }
+        });
+
+    }
+
+    //Method for Retrofit to call the REST API
+    private void Login(String username , String password){
+
+        Call<LoginJSON> log = customersAPI.loginPost(username,password);
+
+        log.enqueue(new Callback<LoginJSON>() {
+            @Override
+            public void onResponse(Call<LoginJSON> call, Response<LoginJSON> response) {
+
+                if(!response.isSuccessful()){
+                    Toast.makeText(LoginActivity.this,"Code : "+response.code(),Toast.LENGTH_LONG).show();
+                    return;
+                }
+                LoginJSON lo = response.body();
+                errorBool = lo.getError();
+                message = lo.getMessage();
+                String user = lo.getUser();
+                Log.i("error: ",errorBool+"\t"+message);
+
+                if(errorBool==false){
+                    startActivity(new Intent(LoginActivity.this,DashBoardActivity.class));
+                    Toast.makeText(LoginActivity.this,"Welocome "+user, Toast.LENGTH_LONG).show();
+                }else{
+                    Toast.makeText(LoginActivity.this,""+message, Toast.LENGTH_LONG).show();
+                }
+
+            }
+
+            @Override
+            public void onFailure(Call<LoginJSON> call, Throwable t) {
+                Toast.makeText(LoginActivity.this,t.getMessage(),Toast.LENGTH_LONG).show();
+            }
+        });
+
+    }
+
+    //Cannot go back from this Activity
+    @Override
+    public void onBackPressed() {
+
+    }
+}
